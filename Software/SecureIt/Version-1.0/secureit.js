89e4855bde4f41defa855550c69bc59eca57c43d
ReMakeEver <evergenbyremake@gmail.com>
2016-08-30 14:38:40 -0600

    Create secureit.js

---
+++ Software/SecureIt/Version-1.0/secureit.js
@@ -0,0 +1,50 @@
+// Lowercase Letter
+var a = "97";var b = "98";var c = "99";var d = "100";
+var e = "101";var f = "102";var g = "103";var h = "104";
+var i = "105";var j = "106";var k = "107";var l = "108";
+var m = "109";var n = "110";var o = "111";var p = "112";
+var q = "113";var r = "114";var s = "115";var t = "116";
+var u = "117";var v = "118";var w = "119";var x = "120";
+var y = "121";var z = "122";
+// Uppercase Letter
+var A = "65";var B = "66";var C = "67";var D = "68";
+var E = "69";var F = "70";var G = "71";var H = "72";
+var I = "73";var J = "74";var K = "75";var L = "76";
+var M = "77";var N = "78";var O = "79";var P = "80";
+var Q = "81";var R = "82";var S = "83";var T = "84";
+var U = "85";var V = "86";var W = "87";var X = "88";
+var Y = "89";var Z = "90";
+// Number Character
+var zero = "48";var one = "49";var two = "50";var three = "51";var four = "52";
+var five = "53";var six = "54";var seven = "55";var eight = "56";var nine = "57";
+var log;
+function theData(){
+ var element = document.getElementByTagName("Password");
+ var value = element.value;
+ var len = x.length;
+ if(len===1){ lenOne(); }if(len===2){ lenTwo(); }if(len===3){ lenThree(); }if(len===4){ lenFour(); }if(len===5){ lenFive(); }if(len===6){ lenSix(); }if(len===7){ lenSeven(); }if(len===8){ lenEight(); }if(len===9){ lenNine(); }if(len===10){ lenTen(); }
+}
+function lenOne(){
+ var element = document.getElementBytagName("Password");
+ var l1 = element.substring(0);
+ if(l1=="a"){log=a;}if(l1=="b"){log=b;}if(l1=="c"){log=c;}if(l1=="d"){log=d;}if(l1=="e"){log=e;}if(l1=="f"){log=f;}if(l1=="g"){log=g;}if(l1=="h"){log=h;}if(l1=="i"){log=i;}if(l1=="j"){log=j;}if(l1=="k"){log=k;}if(l1=="l"){log=l;}if(l1=="m"){log=m;}if(l1=="n"){log=n;}if(l1=="o"){log=o;}if(l1=="p"){log=p;}if(l1=="q"){log=q;}if(l1=="r"){log=r;}if(l1=="s"){log=s;}if(l1=="t"){log=t;}if(l1=="u"){log=u;}if(l1=="v"){log=v;}if(l1=="w"){log=w;}if(l1=="x"){log=x;}if(l1=="y"){log=y;}if(l1=="z"){log=z;}
+ if(l1=="A"){log=A;}if(l1=="B"){log=B;}if(l1=="C"){log=C;}if(l1=="D"){log=D;}if(l1=="E"){log=E;}if(l1=="F"){log=F;}if(l1=="G"){log=G;}if(l1=="H"){log=H;}if(l1=="I"){log=I;}if(l1=="J"){log=J;}if(l1=="K"){log=K;}if(l1=="L"){log=L;}if(l1=="M"){log=M;}if(l1=="N"){log=N;}if(l1=="O"){log=O;}if(l1=="P"){log=P;}if(l1=="Q"){log=Q;}if(l1=="R"){log=R;}if(l1=="S"){log=S;}if(l1=="T"){log=T;}if(l1=="U"){log=U;}if(l1=="V"){log=V;}if(l1=="W"){log=W;}if(l1=="X"){log=X;}if(l1=="Y"){log=Y;}if(l1=="Z"){log=Z;}
+ if(l1=="0"){log=zero;}if(l1=="1"){log=one;}if(l1=="2"){log=two;}if(l1=="3"){log=three;}if(l1=="4"){log=four;}
+}
+function lenTwo(){
+}
+function lenThree(){
+}
+function lenFour(){
+}
+function lenFive(){
+}
+function lenSix(){
+}
+function lenSeven(){
+}
+function lenEight(){
+}
+function lenNine(){
+}
+left of at four
