69103dc0c903b3b3aa844d1eeaf0535d2ce3a7c4
ReMakeEver <evergenbyremake@gmail.com>
2016-09-12 15:26:23 -0600

    Create encryption.js

---
+++ Software/SecureIt/Version-1.0/encryption.js
@@ -0,0 +1,9 @@
+var aplhaUp = document.createElement("SCRIPT");var auSrc = document.createAttribute("src");auSrc.value = "https://www.remakenow.us/Software/SecureIt/Version-1.0/AZ.js";alphaUp.setAttribute(auSrc);
+var alhpaDown = document.createElement("SCRIPT");var adSrc = document.createAttribute("src");adSrc.value = "https://www.remakenow.us/Software/SecureIt/Version-1.0/az.js";alphaDown.setAttribute(adSrc);
+var encryptor = document.createElement("SCRIPT");var eSrc = document.createAttribute("src");eSrc.value = "https://www.remakenow.us/Software/SecureIt/Version-1.0/encryptor.js";
+function addEncryption(){
+ document.body.appendChild(alphaUp);
+ document.body.appendChild(alhpaDown);
+ document.body.appendChild(encryptor);
+}
+addEncryption();
