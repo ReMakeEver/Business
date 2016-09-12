f63429fd24adb61c4a3aa1c30ff7ef58fd6cb8b5
ReMakeEver <evergenbyremake@gmail.com>
2016-09-12 10:37:18 -0600

    Create secureit.js

---
+++ Software/SecureIt/secureit.js
@@ -0,0 +1,2 @@
+var script1 = document.createElement("SCRIPT");var s1Src = document.creaateAttribute("src");s1Src.value = "https://www.remakenow.us/Software/SecureIt/Version-1.0/encrypt.js";script1.setAttributeNode(s1Src);
+var script2 = document.createElement("SCRIPT");var 
