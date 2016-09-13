4415de29686821cbc0b2a4af644e8327ca9b9750
ReMakeEver <evergenbyremake@gmail.com>
2016-09-13 13:26:27 -0600

    Create len2.js

---
+++ Software/SecureIt/Version-1.0/len2.js
@@ -0,0 +1,5 @@
+function len2(word){
+ var theWord = word.substring();
+ if(theWord=="a"){ log=a; }if(theWord=="b"){ log=b; }if(theWord=="c"){ log=c; }if(theWord=="d"){ log=d; }if(theWord=="e"){ log=e; }if(theWord=="f"){ log=f; }if(theWord=="g"){ log=g; }if(theWord=="h"){ log=h; }if(theWord=="i"){ log=i; }if(theWord=="j"){ log=j; }if(theWord=="k"){ log=k; }if(theWord=="l"){ log=l; }if(theWord=="m"){ log=m; }if(theWord=="n"){ log=n; }if(theWord=="o"){ log=o; }if(theWord=="p"){ log=p; }if(theWord=="q"){ log=q; }if(theWord=="r"){ log=r; }if(theWord=="s"){ log=s; }if(theWord=="t"){ log=t; }if(theWord=="u"){ log=u; }if(theWord=="v"){ log=v; }if(theWord=="w"){ log=w; }if(theWord=="x"){ log=x; }if(theWord=="y"){ log=y; }if(theWord=="z"){ log=z; }
+ if(theWord=="A"){ log=A; }if(theWord=="B"){ log=B; }
+}
