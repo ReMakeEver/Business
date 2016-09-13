b03297efa8b6666a29b6c5d3492a7c96079a45f4
ReMakeEver <evergenbyremake@gmail.com>
2016-09-13 14:14:03 -0600

    Delete az.js

--- Software/SecureIt/Version-1.0/az.js
+++
@@ -1,6 +0,0 @@
-var a = 97;var b = 98;var c = 99;var d = 100;var e = 101;
-var f = 102;var g = 103;var h = 104;var i = 105;var j = 106;
-var k = 107;var l = 108;var m = 109;var n = 110;var o = 111;
-var p = 112;var q = 113;var r = 114;var s = 115;var t = 116;
-var u = 117;var v = 118;var w = 119;var x = 120;var y = 121;
-var z = 122;
