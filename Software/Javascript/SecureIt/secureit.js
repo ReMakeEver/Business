da8622140ced7f8a46460b892d74179b2ffec27b
ReMakeEver <evergenbyremake@gmail.com>
2016-09-16 14:31:59 -0600

    Create secureit.js

---
+++ Software/Javascript/SecureIt/secureit.js
@@ -0,0 +1,8 @@
+// Author: L. P. Neil
+// Orginization: ReMakeNow
+// Current Version?: 1.0
+var src1 = "https://remakeever.github.io/Business/Software/Javascript/SecureIt/Version-1.0/encryption.js";
+var script1 = document.createElement("SCRIPT");var s1Src = document.createAttribute("src");s1Src.value = src1;script1.setAttribute(s1Src);
+function startSecurity(){
+ document.body.appendChild(script1);
+}
