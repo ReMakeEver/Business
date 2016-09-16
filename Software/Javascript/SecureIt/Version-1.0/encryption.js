94ff44f1e58532f3814bebafc433742152265459
ReMakeEver <evergenbyremake@gmail.com>
2016-09-16 14:35:57 -0600

    Create encryption.js

---
+++ Software/Javascript/SecureIt/Version-1.0/encryption.js
@@ -0,0 +1,5 @@
+var lowerAlpha = "https://remakeever.github.io/Business/Software/Javascript/SecureIt/Version-1.0/lowAZ.js";
+var lowScript = document.createElement("SCRIPT");var lSSrc = document.createAttribute("src");lSSrc.value = lowerAlpha;lowScript.setAttribute(lSSrc);
+function addEncryption(){
+ document.body.appendChild(lowScript);
+}
